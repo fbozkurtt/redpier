7f43102a870f21b323b54b5d3482782fe1affd5e
fbozkurtt <furkanbozkurt55@gmail.com>
2021-02-23 10:46:41 +0300

    initial commit

---
+++ src/Redpier.Application/Common/Interfaces/IIdentityService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redpier.Application.Common.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<string> GetUserNameAsync(string userId);
+
+        Task<bool> IsInRoleAsync(string userId, string role);
+        Task<bool> AuthorizeAsync(string userId, string policyName);
+        
+    }
+}
---
+++ src/Redpier.Domain/Entities/Container.cs
@@ -0,0 +1,13 @@
+﻿using Redpier.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redpier.Domain.Entities
+{
+    public class Container : BaseEntity
+    {
+    }
+}

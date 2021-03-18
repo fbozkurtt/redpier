ba9839e71320766968af07412c69103a714d1689
fbozkurtt <furkanbozkurt55@gmail.com>
2021-03-18 22:19:25 +0300

    Added a page for adding containers.

---
+++ src/application/Commands/Docker/Containers/CreateContainerCommand.cs
@@ -0,0 +1,41 @@
+﻿using Docker.DotNet;
+using Docker.DotNet.Models;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Redpier.Shared.Constants;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Redpier.Application.Commands.Docker.Containers
+{
+    [Authorize(Roles = DefaultRoleNames.Admin)]
+    public class CreateContainerCommand : IRequest<CreateContainerResponse>
+    {
+        [Required]
+        public string Endpoint { get; set; }
+
+        public CreateContainerParameters Parameters { get; set; }
+    }
+
+    public class CreateContainerCommandHandler : IRequestHandler<CreateContainerCommand, CreateContainerResponse>
+    {
+        private readonly IDockerClient _client;
+
+        public CreateContainerCommandHandler(IDockerClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<CreateContainerResponse> Handle(CreateContainerCommand request, CancellationToken cancellationToken)
+        {
+            return await _client.Containers.CreateContainerAsync(
+                request.Parameters,
+                cancellationToken);
+        }
+    }
+}

671192ba40132c56ad29280c3c02375330e8a03c
fbozkurtt <furkanbozkurt55@gmail.com>
2021-03-24 18:29:26 +0300

    Added console page.

---
+++ src/Redpier.Web.UI/wwwroot/js/app.js
@@ -0,0 +1,41 @@
+﻿function focusElement(id) {
+    const element = document.getElementById(id);
+    element.focus();
+}
+
+function disableElement(id) {
+    const element = document.getElementById(id);
+    element.disabled = true;
+}
+
+function enableElement(id) {
+    const element = document.getElementById(id);
+    element.disabled = false;
+}
+
+function showElement(id) {
+    const element = document.getElementById(id);
+    element.hidden = false;
+}
+
+function clearInput(id) {
+    const element = document.getElementById(id);
+    element.value = "";
+}
+
+function fillElement(id, text) {
+    const element = document.getElementById(id);
+    element.innerText = text;
+}
+
+function clearElement(id) {
+    const element = document.getElementById(id);
+    element.innerText = "";
+}
+
+function addConsoleLine(parentId, id) {
+    const parent = document.getElementById(parentId);
+    let child = document.createElement("input");
+    child.setAttribute("id", id);
+    child.setAttribute("hidden");
+}
